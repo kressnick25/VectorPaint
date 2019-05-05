20d69a6e97c5b93f7035f96ea2d6cbabb4e63382
Nick Kress <kressnick25@gmail.com>
2019-05-05 14:11:53 +1000

    added read method

--- src/Vec.java
+++ src/Vec.java
@@ -1,37 +1,69 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.LinkedList;
 import java.util.PriorityQueue;
+import java.util.Queue;
 
 public class Vec {
-
-    // TODO might need to be different type. a type that command can be altered mid array.
     // PQ == ArrayList with first in last out
-    private PriorityQueue<String> commands;
-
+    // Linked list == Array list with FIFO structure
+    private Queue<String> commands = new LinkedList<>();
+    private String filename;
 
     /**
      * Construct new Vec class to read data from file.
-     * @param file String file
+     * @param filename String file
      */
-    public Vec(String file){
-        // TODO read from filesystem here
+    public Vec(String filename){
+        this.filename = filename;
     }
 
     /**
      * Construct new Vec class to write curent data to file.
+     * @param filename
      * @param commands
      */
-    public Vec(PriorityQueue<String> commands){
-        this.commands = commands;
+    public Vec(String filename, PriorityQueue<String> commands){
+        // Convert PQ to LinkedList and store in private var
+        this.commands = new LinkedList<String>(commands);
     }
 
     public void save(){
         // TODO for each in commands, write to file with newline
+
     }
 
+    /**
+     * Reads from filename stream to private variable;
+     */
     public void read(){
-        // TODO read from file. Dump into commands.
+        Queue<String> lines = new LinkedList<>();
+
+        try {
+            // Open file to new reader
+            BufferedReader reader = new BufferedReader(new FileReader(filename));
+            String line;
+            // For each line, add to lines queue
+            while((line = reader.readLine()) != null){
+                lines.add(line);
+            }
+            // Close file connection
+            reader.close();
+            // Assign lines to private var
+            this.commands = lines;
+        }
+        catch (Exception e){
+            // Print exception
+            System.err.format("Error trying to read '%s'.", filename);
+            e.printStackTrace();
+        }
     }
 
-    public PriorityQueue<String> get(){
+    /**
+     *
+     * @return Queue of commands
+     */
+    public Queue<String> get(){
         return commands;
     }
 
--- src/component/Drawable.java
+++ src/component/Drawable.java
@@ -13,7 +13,6 @@ public interface Drawable {
      * @return Command in format: LINE 0.0 0.1 2.4 2.7
      */
     String toString();
-
     /**
      *
      * @return Type as a string: RECTANGLE
