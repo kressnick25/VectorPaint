dd7b174cf75daa14ad88a88353b1d3e5269d9b4a
Anton <lithium610@hotmail.com>
2019-05-07 17:49:35 +1000

    PlotTest complete - added random Int's

--- src/component/PlotTest.java
+++ src/component/PlotTest.java
@@ -1,16 +1,18 @@
 package component;
 import component.Plot;
 import org.junit.jupiter.api.*;
+import java.util.Random;
 
-import java.awt.*;
-import java.util.ArrayList;
 import java.util.StringJoiner;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class PlotTest {
     Plot plotTest;
-    VectorPoint pointTestLocation = new VectorPoint (0.9, 0.9);
+    Random random = new Random();
+    int randomInt = random.nextInt(100);
+    int randomInt2 = random.nextInt(100);
+    VectorPoint pointTestLocation = new VectorPoint (randomInt, randomInt2);
     private static Drawable.Type typeTest;
 
     @Test
