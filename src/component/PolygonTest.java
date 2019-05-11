ce78420e8cd10a4d97cdffa291de4d8b398da586
Callum <n10225684@qut.edu.au>
2019-05-11 15:06:41 +1000

    Need confirmation on toString VectorPoint or Point.

--- src/component/PolygonTest.java
+++ src/component/PolygonTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.awt.*;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -24,22 +25,43 @@ class PolygonTest {
         thrown = null;
         arrayPoint = null;
     }
-    @Test
-    void constructorTest(){
+    ArrayList arrayInitializer(){
         ArrayList<VectorPoint> arrayPoint = new ArrayList<VectorPoint>();
 
         VectorPoint pointTest = new VectorPoint(0.6,0.5);
         arrayPoint.add(pointTest);
+        return arrayPoint;
+    }
+    @Test
+    void constructorTest(){
+        ArrayList<VectorPoint> arrayPoint = arrayInitializer();
 
         polygenTest = new Polygon(arrayPoint);
         assertEquals(arrayPoint, polygenTest.getArrayPointsList());
     }
     @Test
     void getArrayPointsListTest(){
-        ArrayList<VectorPoint> arrayPoint = new ArrayList<VectorPoint>();
-        VectorPoint pointTest = new VectorPoint(0.6,0.5);
-        arrayPoint.add(pointTest);
+        ArrayList<VectorPoint> arrayPoint = arrayInitializer();
+
         polygenTest = new Polygon(arrayPoint);
         assertEquals(arrayPoint, polygenTest.getArrayPointsList());
     }
+    @Test
+    void getTypeTest() {
+        ArrayList<VectorPoint> arrayPoint = arrayInitializer();
+
+        polygenTest = new Polygon(arrayPoint);
+        assertEquals(Drawable.Type.POLYGON, polygenTest.getType());
+    }
+    @Test
+    void toStringTest() {
+        ArrayList<VectorPoint> arrayPoint = arrayInitializer();
+        //TODO Need to confirm if Point or VectorPoint
+        polygenTest = new Polygon(arrayPoint);
+
+
+
+        assertEquals(1, 1);
+    }
+
 }
